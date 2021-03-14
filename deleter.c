50c782e0262b98a9557dfc853878e4de3325658d
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-03-14 14:50:33 +0300

    Expanding the stack and heap increase the amount of memory allocated to the stack and heap with pragma directives

--- deleter.c
+++ deleter.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#pragma comment(linker, "/STACK:2000000")
+#pragma comment(linker, "/HEAP:2000000")
 
 int main(int argc, char* argv[]) {
 	if (argc != 3) return puts("Specify the configuration file and the text file");
