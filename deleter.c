09be7684483231824cc38c2e941061a36b007ed9
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-03-14 14:40:25 +0300

    Added opening files by paths from launch arguments

--- deleter.c
+++ deleter.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
-void main(void) {
+int main(int argc, char* argv[]) {
+	if (argc != 3) return puts("Specify the configuration file and the text file");
 
+	FILE* configFile = fopen(argv[1], "r+");
+	FILE* textFile = fopen(argv[2], "w+");
 }
