e44f3555790471dfb0be22135452535bbef61a9e
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-03-14 15:50:35 +0300

    Implemented reading text from a file

--- deleter.c
+++ deleter.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
 #pragma comment(linker, "/STACK:2000000")
-#pragma comment(linker, "/HEAP:2000000")
+#pragma comment(linker, "/HEAP:5000000")
+
+#define MAX_FILE_LENGTH 2500000
+#define READ_MAX_LEN 1024
 
 int main(int argc, char* argv[]) {
 	if (argc != 3) return puts("Specify the configuration file and the text file");
 
 	FILE* configFile = fopen(argv[1], "r+");
-	FILE* textFile = fopen(argv[2], "w+");
+	FILE* textFile = fopen(argv[2], "r+");
+
+	char* text = (char*)calloc(MAX_FILE_LENGTH, sizeof(char));
+	for (size_t i = 0; i < MAX_FILE_LENGTH && !feof(textFile); i++) {
+		text[i] = fgetc(textFile);
+	}
 }
