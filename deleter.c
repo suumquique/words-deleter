a50206f913bab6cf9375901c16408b919355b633
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-03-15 19:05:10 +0300

    Implemented work with the config using the stack Now all directives (commands) from the configuration file are added to the sorted unidirectional list. First, those are added where it is required to remove words from the beginning of the text in the main file, those lines that will be removed from the end of the text go to the end of the stack.

--- deleter.c
+++ deleter.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
 
 #pragma comment(linker, "/STACK:2000000")
 #pragma comment(linker, "/HEAP:6000000")
@@ -9,8 +12,24 @@
 #define MAX_FILE_LENGTH 5000000
 // Максимальная длина предложения из конфигурационного файла, которое требуется удалить из текста
 #define MAX_SENTENCE_LENGTH 2048
-// Максимальное количество команд (предложений) в конфигурационном файле
-#define MAX_STRING_NUMBER 100
+
+// Константа, показывающая, что требуется удалить все совпадения
+#define ALL INT_MAX
+
+/* Некая смесь двух структур: сортированный однонаправленный список и стек.
+В начало добавляются директивы с параметром reverse = false, в конец идут с reverse = true.
+Затем с помощью функции pop() вынимаются элементы сверху стека, то есть сначала идут все директивы с reverse = true */
+typedef struct stackNode{
+	char* sentence;
+	size_t count;
+	bool reverse;
+	struct stackNode* ptr_next;
+} sortedStackNode;
+
+void insert(sortedStackNode** start_ptr, const char* string, size_t count, bool reverse);
+sortedStackNode* pop(sortedStackNode** top_ptr);
+sortedStackNode* getDirectivesFromConfigFile(FILE* configFile, sortedStackNode* startStackPtr);
+
 
 int main(int argc, char* argv[]) {
 	if (argc != 3) return puts("Specify the configuration file and the text file");
@@ -26,11 +45,87 @@ int main(int argc, char* argv[]) {
 	// Скидываем указатель назад на начало файла
 	rewind(textFile);
 
-	char linesToDelete[MAX_STRING_NUMBER][MAX_SENTENCE_LENGTH];
-	for (size_t i = 0; i < MAX_STRING_NUMBER && !feof(configFile); i++) {
-		fgets(linesToDelete[i], MAX_SENTENCE_LENGTH - 1, configFile);
-		char* lastSymbolPtr = &linesToDelete[i][strlen(linesToDelete[i]) - 1];
+	sortedStackNode* directivesStackTopPtr = NULL;
+	directivesStackTopPtr = getDirectivesFromConfigFile(configFile, directivesStackTopPtr);
+	print_stack(directivesStackTopPtr);
+}
+
+sortedStackNode* getDirectivesFromConfigFile(FILE* configFile, sortedStackNode* startStackPtr) {
+	char directive[MAX_SENTENCE_LENGTH];
+	bool reverse = false;
+	size_t count, numberFromString, i;
+
+	while (!feof(configFile)) {
+		// Читаем строку для удаления из файла
+		fgets(directive, MAX_SENTENCE_LENGTH - 1, configFile);
+		// Если последний символ строки считался как перенос строки, то удаляем перенос и ставим на его место нуль-терминатор
+		char directiveLen = strlen(directive);
+		char* lastSymbolPtr = &directive[directiveLen - 1];
 		if (*lastSymbolPtr == '\n') *lastSymbolPtr = '\0';
-		puts(linesToDelete[i]);
+		// Если первый символ команды - решетка, то это значит, что удалять требуется с конца
+		reverse = directive[0] == '#' ? true : false;
+		
+		/* С помощью функции atoi получаем из директивы число (сколько раз нужно удалить указанное слово/команду).
+		* Если числа нет, или не указан идентификатор (считать с начала или с конца), то считаем, что требуется удалить все совпадения.*/
+		if (directive[0] == '#' || directive[0] == '^') { 
+			numberFromString = atoi(&directive[1]);
+			count = numberFromString ? numberFromString : 1;
+		}
+		else count = ALL;
+		
+		/* Вычленяем из директивы (команду) саму строку/слово для удаления, убирая первые i символов, на месте которых
+		* стоят командные символы - # и ^ и дальше числа */
+		for (i = 0; (isdigit(directive[i]) && i) || (i == 0 && (directive[0] == '#' || directive[0] == '^')); i++);
+		char* currentLineToDelete = (char*)malloc((directiveLen + 1) * sizeof(char));
+		strcpy(currentLineToDelete, &directive[i]);
+		// Добавляем результат в наш стек-список
+		insert(&startStackPtr, currentLineToDelete, count, reverse);
 	}
+
+	return startStackPtr;
+}
+
+void insert(sortedStackNode** start_ptr, const char* string, size_t count, bool reverse) {
+	sortedStackNode* new_ptr = NULL, * prev_ptr = NULL, * cur_ptr = *start_ptr;
+
+	new_ptr = (sortedStackNode*)calloc(1, sizeof(sortedStackNode));
+
+	if (new_ptr != NULL)
+	{
+		new_ptr->sentence = string;
+		new_ptr->count = count;
+		new_ptr->reverse = reverse;
+		new_ptr->ptr_next = NULL;
+
+		while (cur_ptr != NULL && reverse > cur_ptr->reverse)
+		{
+			prev_ptr = cur_ptr;
+			cur_ptr = cur_ptr->ptr_next;
+		}
+
+		if (prev_ptr == NULL)
+		{
+			new_ptr->ptr_next = *start_ptr;
+			*start_ptr = new_ptr;
+		}
+		else
+		{
+			prev_ptr->ptr_next = new_ptr;
+			new_ptr->ptr_next = cur_ptr;
+		}
+	}
+}
+
+sortedStackNode* pop(sortedStackNode** top_ptr) {
+	sortedStackNode* temp_ptr = NULL;
+
+	if (*top_ptr == NULL)
+	{
+		return NULL;
+	}
+
+	temp_ptr = *top_ptr;
+	*top_ptr = (*top_ptr)->ptr_next;
+
+	return temp_ptr;
 }
